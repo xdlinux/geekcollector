5089ae2b527dc923a18305dbe67234ce610a5719
shanzi <yun.er.run@gmail.com>
2011-09-06 11:51:31 +0800

    将member和routes托管到admin后台

---
+++ members/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from xdlinux.members.models import Member,Group
+
+admin.site.register(Member)
+admin.site.register(Group)
+
---
+++ routes/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from xdlinux.routes.models import OS
+
+
+admin.site.register(OS)
+
