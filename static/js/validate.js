1921ddcc039f562029e3fc4c91fd95b6fdd3bb93
shanzi <yun.er.run@gmail.com>
2011-09-08 00:10:57 +0800

    表单方面的js基本上全部完成，得抓紧赶进度了

---
+++ static/js/validate.js
@@ -0,0 +1,57 @@
+$.fn.extend({
+    'validate':function(re){
+        return $(this).val().match((re?re:$(this).data('re')));
+    },
+    'add_validate':function(type,callback)
+   {
+    var re;
+    if(typeof(type)=='undefined') type='email';
+    else{
+        var regexp={
+            'name':/.{2,20}/,
+            'email':/([\w\d_\.]+)@(([\w\d]+\.)+\w{1,5})/,
+            'mobile':/(\+\d\d|00\d\d)?(\d{11})/,
+            'twitter':/@[\w\d_]+/,
+            'student_number' : /(0\d|10|12|13|14)(08|09|10|11)([1-9]\d)(\d\d)/,
+        }
+        if(!(re=regexp[type]))
+        {
+            re=regexp['email'];
+            type='email';
+        }
+    }
+   $(this).data({'re':re})
+    var vali=function(){
+        var match=$(this).validate();
+        if($(this).val().length==0){
+            if($(this).data('required'))
+                callback.call($(this), 'required',null,null);
+            else
+                callback.call($(this), 'empty',null,null);
+        }
+        else if(match) callback.call($(this),'valid',type,match);
+        else  callback.call($(this),'invalid',type,$(this).val())
+    }
+    return $(this)
+        .focus(vali).blur(
+                function(){
+                    if((!$(this).data('required')) && $(this).val()=="") callback.call($(this),'normal')
+                    else vali.call($(this));
+                }
+                )
+        .bind('keyup',vali)
+        .bind('change',vali);
+   },
+    'required':function(bool){
+        if(typeof(bool)!='undefined')
+                return $(this).data({'required':bool});
+        else return $(this).data('required');
+    },
+    'number_only':function(){
+        $(this).bind('keydown',function(e)
+                {
+                    if(!((e.keyCode>=48 && e.keyCode<=58 )|| (e.keyCode>=35 && e.keyCode<=37)|| e.keyCode==39 ||e.keyCode==8 ||e.keyCode==9))return false;
+                    return true;
+                }).bind('afterpaste',function(){$(this).val($(this).val().replace(/\D/g,''))});
+    }
+})
