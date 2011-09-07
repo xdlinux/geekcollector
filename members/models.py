912b17bd490de40430de2be2e3ba46654bbbc6a5
Sean Lee <iseansay@gmail.com>
2011-09-07 09:12:55 +0800

    added twitter_id filed in Member, and student_num should be unique

--- members/models.py
+++ members/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Group(models.Model):
     """小组，包括WDT，android，Video，Newbie"""
-    name = models.CharField(max_length=20)
-    describe = models.CharField(max_length=140, blank=True) #小组简介限制字数一百四，大家都懂的，回来让各个小组的负责人写，WDT的畜松你自己搞定吧~
+    name = models.CharField(max_length=30)    
+    describe = models.TextField(max_length=140, blank=True) #小组简介限制字数一百四，大家都懂的，回来让各个小组的负责人写，WDT的畜松你自己搞定吧~
     def __unicode__(self):
         return self.name
 
@@ -14,10 +14,10 @@ class Member(models.Model):
     """成员的DRM"""
     #基本信息
     name = models.CharField(max_length=10)
-    student_num = models.IntegerField()
+    student_num = models.CharField(max_length=15, unique=True)
     mobile = models.CharField(max_length=20)
     email = models.EmailField(max_length=75, blank=True)
-    twitter_id = models.TextField(max_length=30, blank=True)
+    twitter_id = models.CharField(max_length=30, blank=True)
     #小组选择信息，每人可以选择多个小组，设定related_name为groups就可以通过Group.members直接访问某个小组所有报名的人了
     groups = models.ForeignKey(Group, related_name='members')
 
