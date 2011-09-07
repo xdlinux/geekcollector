f2162dbb770517506297af492fa9aa5f8db5442e
Sean Lee <iseansay@gmail.com>
2011-09-07 13:21:11 +0800

    return a extra error if the student_num is already in database

--- members/views.py
+++ members/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from xdlinux.members.models import Member, Group #引入报名成员的CRM，members，具体实现已经完成，请看源文件，注释都写了
+from django.db import IntegrityError
 
 def count(request):
     """返回当前已经报名的总人数，直接返回数字就好了，不需要特殊格式"""
@@ -21,13 +22,17 @@ def signup(request):
 
     记得GET和POST分开处理哦~
     """
+    groups=Group.objects.all()
     if request.method == 'GET':
-        groups=Group.objects.all()
         return render_to_response('signup.html', {'groups':groups})
     elif request.method == 'POST':
         group_id = int(request.POST['groups'])
         group = Group.objects.get(id = group_id)
-        Member.objects.create(name = request.POST['name'], student_num = request.POST['student_num'], mobile = request.POST['mobile'],email = request.POST['email'], groups= group, keywords = request.POST['keywords'],)
-        num = Member.objects.count()
-        return render_to_response('signup_callback.html', locals()) #用locals()可以直接将当前环境中所有的变量传入
+        try:
+            Member.objects.create(name = request.POST['name'], student_num = request.POST['student_num'], mobile = request.POST['mobile'],email = request.POST['email'], groups= group, keywords = request.POST['keywords'],)
+        except  IntegrityError: # if the student_num is already in database, return a extra error
+            return render_to_response('signup.html', {'groups':groups, 'error':'你输入的学号已经注册'})
+        else:
+            num = Member.objects.count()
+            return render_to_response('signup_callback.html', locals()) #用locals()可以直接将当前环境中所有的变量传入
 
