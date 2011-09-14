8d4979671b2a52de63f46f3da3b9a8643d02665b
shanzi <yun.er.run@gmail.com>
2011-09-14 10:07:55 +0800

    修改routes model 输出原有数据，重置数据库

--- routes/models.py
+++ routes/models.py
@@ -5,7 +5,7 @@ class OS(models.Model):
     """不同操作系统的展示信息"""
     name = models.CharField(max_length=20)
     describe = models.CharField(max_length=140)
-    image = models.CharField(max_length=60)
+    image = models.ImageField(upload_to='media/', max_length=100)
     def __unicode__(self):
         return self.name
 
