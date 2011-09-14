8cda7a6c6364c16ba0dff2289bb0fe195c04d728
shanzi <yun.er.run@gmail.com>
2011-09-14 10:03:07 +0800

    将count页面设为登陆后才能访问

--- members/views.py
+++ members/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render_to_response, redirect
 from django.http import HttpResponse
 from xdlinux.members.models import Member, Group #引入报名成员的CRM，members，具体实现已经完成，请看源文件，注释都写了
 from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def count(request):
     """返回当前已经报名的总人数，直接返回数字就好了，不需要特殊格式"""
     sumcount = '%d' % Member.objects.count()
@@ -15,18 +17,6 @@ def count(request):
     
     
 def signup(request):
-    """
-    渲染报名表的view，并处理报名表单的提交
-    表单比较简单，因此不再使用django的表单
-
-    表单验证方面，畜松帮我寻找一下有没有好的jquery验证神马的就好
-
-    用render_to_response可以直接渲染模板
-    signup.html是一开始呈现的页面
-    signup_callback.html 是提交表单且通过之后的反馈页面，需要显示当前是第几个报名的
-
-    记得GET和POST分开处理哦~
-    """
     groups=Group.objects.all()
     if request.method == 'GET':
         return render_to_response('signup.html', {'groups':groups})
